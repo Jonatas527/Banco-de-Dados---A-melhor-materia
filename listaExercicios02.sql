bdfd2f06b8358ff63af55226355cb79150c93eee
Jonatas527 <102703755+Jonatas527@users.noreply.github.com>
2023-09-26 20:07:57 -0300

    commit6

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -49,5 +49,45 @@ DELIMITER ;
 CALL sp_ContarLivrosPorCategoria('Ciência', @total);
 SELECT @total AS 'Total de Livros';
 
+4. *Verificação de Livros por Categoria*:
+
+sql
+DELIMITER //
+
+CREATE PROCEDURE sp_VerificarLivrosCategoria(IN categoriaNome VARCHAR(100), OUT possuiLivros BOOLEAN)
+BEGIN
+    DECLARE total INT;
+    SELECT COUNT(*) INTO total
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+
+    IF total > 0 THEN
+        SET possuiLivros = TRUE;
+    ELSE
+        SET possuiLivros = FALSE;
+    END IF;
+END //
+
+DELIMITER ;
+
+CALL sp_VerificarLivrosCategoria('Autoajuda', @temLivros);
+SELECT IF(@temLivros, 'Possui Livros', 'Não Possui Livros') AS 'Categoria';
+
+5. *Listagem de Livros por Ano*:
+
+sql
+DELIMITER //
+
+CREATE PROCEDURE sp_LivrosAteAno(IN anoLimite INT)
+BEGIN
+    SELECT Titulo
+    FROM Livro
+    WHERE Ano_Publicacao <= anoLimite;
+END //
+
+DELIMITER ;
+
+CALL sp_LivrosAteAno(2010);
 
 
