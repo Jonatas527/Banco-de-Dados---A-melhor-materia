ce9a6275ef3550f7c180525c790e684fbfe5d899
Jonatas527 <102703755+Jonatas527@users.noreply.github.com>
2023-09-26 20:11:20 -0300

    commit 10

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -182,4 +182,19 @@ END //
 
 DELIMITER ;
 
+10. *Livros e Seus Autores*:
 
+sql
+DELIMITER //
+
+CREATE PROCEDURE sp_LivrosESeusAutores()
+BEGIN
+    SELECT Livro.Titulo, CONCAT(Autor.Nome, ' ', Autor.Sobrenome) AS 'Autor'
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
+END //
+
+DELIMITER ;
+
+CALL sp_LivrosESeusAutores();
