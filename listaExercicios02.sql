4a7f41dd45cdc2f0179bc206964ec248df32d005
Jonatas527 <102703755+Jonatas527@users.noreply.github.com>
2023-09-26 20:04:07 -0300

    commit1

---
+++ listaExercicios02.sql
@@ -0,0 +1,14 @@
+1. *Listagem de Autores*:
+
+sql
+DELIMITER //
+
+CREATE PROCEDURE sp_ListarAutores()
+BEGIN
+    SELECT * FROM Autor;
+END //
+
+DELIMITER ;
+
+-- Teste da stored procedure
+CALL sp_ListarAutores();
