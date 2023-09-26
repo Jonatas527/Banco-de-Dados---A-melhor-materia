3d7c5c7cf7e4990a3efe4bda555d0431d61b710b
Jonatas527 <102703755+Jonatas527@users.noreply.github.com>
2023-09-26 20:06:37 -0300

    commit 3

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -1,4 +1,4 @@
-1. *Listagem de Autores*:
+1. Listagem de Autores:
 
 sql
 DELIMITER //
@@ -10,10 +10,10 @@ END //
 
 DELIMITER ;
 
--- Teste da stored procedure
+
 CALL sp_ListarAutores();
 
-2. *Livros por Categoria*:
+2. Livros por Categoria:
 
 sql
 DELIMITER //
@@ -28,5 +28,26 @@ END //
 
 DELIMITER ;
 
--- Teste da stored procedure
+
 CALL sp_LivrosPorCategoria('Romance');
+
+3. *Contagem de Livros por Categoria*:
+
+sql
+DELIMITER //
+
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoriaNome VARCHAR(100), OUT totalLivros INT)
+BEGIN
+    SELECT COUNT(*) INTO totalLivros
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+
+DELIMITER ;
+
+CALL sp_ContarLivrosPorCategoria('Ciência', @total);
+SELECT @total AS 'Total de Livros';
+
+
+
